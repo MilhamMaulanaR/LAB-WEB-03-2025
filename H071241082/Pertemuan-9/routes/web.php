19fc83b871e07c55e43163e4ac43c95ae1a70bf3
Ryannfirmansyah <ryannnfirmannsyah@gmail.com>
2025-11-21 21:49:13 +0800

    kumpul tp 6,7,8,9

---
+++ H071241082/Pertemuan-9/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\WarehouseController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StockController;
+
+
+Route::get('/', function () {
+    return redirect('products'); // atau categories / stocks
+});
+
+
+Route::resource('categories', CategoryController::class);
+Route::resource('warehouses', WarehouseController::class);
+Route::resource('products', ProductController::class);
+
+Route::get('stocks', [StockController::class, 'index'])->name('stocks.index');
+Route::get('stocks/transfer', [StockController::class, 'create'])->name('stocks.create');
+Route::post('stocks/transfer', [StockController::class, 'store'])->name('stocks.store');
